bc232f280c5d927dc979ff5b3941d63134fe3ecd
Anastaska1209 <69230440+Anastaska1209@users.noreply.github.com>
2020-08-10 11:33:33 +0500

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include < math.h >
+#include<stdbool.h>
+
+
+
+int main() {
+	float A,B,C,D;
+	
+	scanf_s("%lf%lf%lf%lf", &A,&B,&C,&D);
+	
+
+	printf("%d\n",(A/B>=C/D));
+	
+
+
+	return 0;
+
+}
