92a36234eaf0d14f809af60c1fab36977c9ed0a5
Anastaska1209 <69230440+Anastaska1209@users.noreply.github.com>
2020-08-10 11:14:57 +0500

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include < math.h >
+#include<stdbool.h>
+
+
+
+int main() {
+	int a;
+	
+	scanf_s("%d", &a);
+	
+
+	printf("%d\n",(a>=1000&&a<=9999&&a!=4999));
+	
+
+
+	return 0;
+
+}
